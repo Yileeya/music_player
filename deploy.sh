572f4043c540d426793754f16ad540721ffb3139
Yilee <rita841027@gmail.com>
2021-02-12 23:52:32 +0800

    新增排行及音樂modal

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,5 @@ git init
 #//因為dist資料夾預設是被ignore的因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
 git push -f https://github.com/Yileeya/music_player.git master:gh-pages
-#//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 //vue.config.js
 module.exports = {
+    devServer: {
+        proxy: {
+            '/token': {
+                target: 'https://account.kkbox.com/oauth2/token',
+                ws: true,
+                changeOrigin: true,
+            },
+        },
+    },
     publicPath: process.env.NODE_ENV === 'production'
         ? '/music_player/'
         : '/'
